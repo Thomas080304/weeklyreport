007739a6ffc0e6231e21d5b545a023242ade2122
yangtao <yangtao@easted.com.cn>
2018-11-27 17:30:03 +0800

    mvc model

--- 11.18/checkit/src/js/models/TodoModel.js
+++ 11.18/checkit/src/js/models/TodoModel.js
@@ -1,4 +1,35 @@
-maria.Model.subclass(checkit, 'TodoModel', {
+checkit.TodoModel = function() {
+    maria.Model.apply(this, arguments);
+};
+checkit.TodoModel.superConstructor = maria.Model;
+checkit.TodoModel.prototype = maria.create(maria.Model.prototype);
+checkit.TodoModel.prototype.constructor = checkit.TodoModel;
+checkit.TodoModel.prototype._content = '';
+checkit.TodoModel.prototype._isDone = false;
+checkit.TodoModel.prototype.getContent = function() {
+    return this._content;
+};
+checkit.TodoModel.prototype.setContent = function(content) {
+    content = checkit.trim('' + content);
+    if (this._content !== content) {
+        this._content = content;
+        this.dispatchEvent({type: 'change'});
+    }
+};
+checkit.TodoModel.prototype.isDone = function() {
+    return this._isDone;
+};
+checkit.TodoModel.prototype.setDone = function(isDone) {
+    isDone = !!isDone;
+    if (this._isDone !== isDone) {
+        this._isDone = isDone;
+        this.dispatchEvent({type: 'change'});
+    }
+};
+checkit.TodoModel.prototype.toggleDone = function() {
+    this.setDone(!this.isDone());
+};
+/* maria.Model.subclass(checkit, 'TodoModel', {
     properties: {
         _content: '',
         _isDone: false,
@@ -26,4 +57,4 @@ maria.Model.subclass(checkit, 'TodoModel', {
             this.setDone(!this.isDone());
         }
     }
-});
+}); */
