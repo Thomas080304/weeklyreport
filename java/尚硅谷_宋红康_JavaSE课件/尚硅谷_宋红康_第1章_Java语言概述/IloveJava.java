40157bee23aadc530ee4564e08f589a447e1d02b
yangtao <yangtao@easted.com.cn>
2019-02-20 21:39:15 +0800

    doc for java and book

---
+++ java/尚硅谷_宋红康_JavaSE课件/尚硅谷_宋红康_第1章_Java语言概述/IloveJava.java
@@ -0,0 +1,18 @@
+
+//love Java¿‡√˚
+class IloveJava{
+
+	public static void main(String [] args){
+		
+		System.out.print("\t"+"*"+"\t\t\t\t\t\t\t\t\t\t\t\t"+"*"+"\t"+"\n");
+		System.out.print("*"+"\t\t"+"*"+"\t\t\t\t"+"I love Java"+"\t\t\t\t"+"*"+"\t\t"+"*"+"\n");
+		System.out.print("\t"+"*"+"\t\t\t\t\t\t\t\t\t\t\t\t"+"*"+"\t"+"\n");
+		System.out.print("\t\t"+"*"+"\t\t\t\t\t\t\t\t\t\t"+"*"+"\t\t"+"\n");
+		System.out.print("\t\t\t"+"*"+"\t\t\t\t\t\t\t\t"+"*"+"\t"+"\n");
+		System.out.print("\t\t\t\t"+"*"+"\t\t\t\t\t\t"+"*"+""+"\t"+"\n");
+		System.out.print("\t\t\t\t\t"+"*"+"\t\t\t\t"+"*"+""+"\t\t"+"\n");
+		System.out.print("\t\t\t\t\t\t"+"*"+"\t\t"+"*"+""+"\t\t"+"\n");
+		System.out.print("\t\t\t\t\t\t\t"+"*"+"\n");
+
+	}
+}
---
+++ java/尚硅谷_宋红康_JavaSE课件/尚硅谷_宋红康_第1章_Java语言概述/PrintGrap.java
@@ -0,0 +1,87 @@
+class PrintGrap{
+	public static void main(String[] args){
+		
+		System.out.print("\t");
+		System.out.print("*");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		
+		System.out.println("*");
+
+		
+		System.out.print("*");
+		System.out.print("\t");
+		//System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("I love java");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		
+		System.out.println("*");
+
+		
+		System.out.print("\t");
+		System.out.print("*");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		
+		System.out.println("*");
+
+		
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("*");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		
+		System.out.println("*");
+
+		
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("*");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		
+		System.out.println("*");
+		
+		
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("*");
+		System.out.print("\t");
+		
+		System.out.println("*");
+
+		
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		//System.out.print("\t");
+		System.out.print("      ");
+		System.out.print("*");
+
+
+	}
+
+}
